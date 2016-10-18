cc4d4325d8a7b17266404fc2e19d5ca4f475fe43
Dark Swordsman <kylebarr54321@gmail.com>
2016-10-17 21:08:19 -0400

    Added content to Home Page

--- app/src/pages/Home/Home.js
+++ app/src/pages/Home/Home.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
+import Button from 'react-bootstrap/lib/Button';
+import Jumbotron from 'react-bootstrap/lib/Jumbotron';
 import PageHeader from 'react-bootstrap/lib/PageHeader';
+
+import './Home.css'
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <PageHeader>guava</PageHeader>
+      <div className="container">
+        <Jumbotron>
+          <h1>Welcome to guava!</h1>
+          <div className="jumbotronOne">
+            <p>guava lets you do all sorts of complex math with ease.</p>
+            <Button bsStyle="primary" bsSize="large">Get Started</Button>
+          </div>
+        </Jumbotron>
       </div>
     );
   }
